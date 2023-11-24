617734ccc34b05a53e7b3a0914f5c3bf70c4a88e
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-11-24 13:23:27 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,3 +3,14 @@
 package { 'python3-pip':
   ensure => installed,
 }
+# install_flask.pp
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => '/usr/bin',
+  creates => '/usr/local/lib/python3.8/dist-packages/flask',
+}
