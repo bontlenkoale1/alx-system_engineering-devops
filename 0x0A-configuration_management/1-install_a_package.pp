8aac3ac04b941dc7a11fc8edeead17ea53c486fb
bontlenkoale1 <bontlenkoale20@gmail.com>
2023-11-25 10:13:58 -0800

    update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,12 +1,11 @@
-/usr/bin/pup
-# using  puppet to install flask from pip3
+# Using Puppet to install Flask and Werkzeug using pip3
 
-package::pip { 'flask' :
-  ensure       => '2.1.0' ,
-  pip_provider => 'pip3' ,
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
 
-package { 'werkzeug' :
-	ensure => '2.1.1'
-	provider => 'pip3'
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
 }
