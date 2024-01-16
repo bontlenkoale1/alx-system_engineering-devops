3d2a32602384bffd4f01e5f9082d6f0ffda0c9bd
bontlenkoale1 <bontlenkoale20@gmail.com>
2024-01-16 14:05:34 -0800

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,32 +2,16 @@
 """Function to query subscribers on a given Reddit subreddit."""
 import requests
 
-def number_of_subscribers(subreddit):
-    """Return the number of subscribers for a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
 
+def number_of_subscribers(subreddit):
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0"
+    }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
 
-    try:
-        # Extract the number of subscribers from the JSON response
-        subscribers = response.json()["data"]["subscribers"]
-        return subscribers
-    except KeyError:
-        # Handle unexpected JSON structure
-        return 0
-
-if __name__ == "__main__":
-    
-import sys
-
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit_name = sys.argv[1]
-        subscribers_count = number_of_subscribers(subreddit_name)
-        print("{:d}".format(subscribers_count))
